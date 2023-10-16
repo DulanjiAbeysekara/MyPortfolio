7287a881e7cc988ff79f92630e328599336de084
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2023-10-16 20:37:44 +0530

    cvbn

--- assignments/JS/InMemoryPOS/db/db.js
+++ assignments/JS/InMemoryPOS/db/db.js
@@ -8,3 +8,8 @@ var ItemDetails = [
     {itemId: "I001", itemName: "ice-cream", qty: "180g", price: "180.22"},
     {itemId: "I002", itemName: "milk", qty: "500g", price: "500.00"}
 ];
+
+var orderDetails = [
+    // {itemId: "I001", itemName: "ice-cream", qty: "180g", price: "180.22"},
+    // {itemId: "I002", itemName: "milk", qty: "500g", price: "500.00"}
+];
